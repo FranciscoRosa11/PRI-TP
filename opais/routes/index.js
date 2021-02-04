86daec54a82b7292b244dee9d41d720b12de8ce5
Francisco Rosa <a80749@alunos.uminho.pt>
2021-02-04 23:51:55 +0000

    register

--- opais/routes/index.js
+++ opais/routes/index.js
@@ -8,8 +8,36 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/protegida', verificaAutenticacao, function (req,res) {
+router.get('/login', function(req,res) {
+  res.render('login-form')
+})
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  
+  axios.post('http://localhost:7710/users/login', req.user)
+    .then(dados => {
+      res.cookie('token', dados.data.token, {
+        maxAge : new Date(Date.now() + 3600000),
+        secure: false, // set to true if your using https
+        httpOnly: true
+      });
+      //res.redirect('/mainPage')
+    })
+    .catch(e => res.render('login-form', {erro: e, user: req.body.username}))
+});
+
+/*router.get('/protegida', verificaAutenticacao, function (req,res) {
   res.render('protegida', {utilizador: req.user.id})
+})*/
+
+router.get('/register', function(req,res) {
+  res.render('register-form', {user: "", email: "", fil: ""})
+})
+
+router.post('/register', function(req,res) {
+  axios.post('http://localhost:7710/users/registar', req.body)
+    .then(dados => res.redirect('/'))
+    .catch(e => res.render('register-form', {error:e, user: req.body.username, email: req.body.email, fil: req.body.filiacao}))
 })
 
 function verificaAutenticacao(req, res, next){
